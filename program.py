adb3204da4e298ec9c6b64fa65d4342abe3b9040
Pun <impan_1611_2544@hotmail.com>
2024-01-27 15:19:00 +0700

    first commit

---
+++ program.py
@@ -0,0 +1,19 @@
+from tkinter import *
+
+root=Tk()
+root.title("Program temperature converter")
+root.iconbitmap("icon/tempicon.ico")
+root.resizable(0, 0)
+
+#settings
+font = ("Arial", 15, "bold")
+color = "orange"
+#input widget
+
+#combobox widget
+
+#output widget
+
+#button widget
+
+root.mainloop()
