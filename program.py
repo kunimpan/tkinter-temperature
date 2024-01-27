9b6131c9e8cf5e698b4be55047c4edb41d3de985
Pun <impan_1611_2544@hotmail.com>
2024-01-27 15:43:00 +0700

    Add:combobox widget

--- program.py
+++ program.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter import ttk
 
 root=Tk()
 root.title("Program temperature converter")
@@ -10,13 +11,19 @@ font = ("Arial", 15, "bold")
 color = "orange"
 
 #input widget
-input_label = Label(root, text="temperature(C)", font=font)
+input_label = Label(root, text="Temperature(C)", font=font)
 input_txt = Entry(root, width=20, font=font)
-input_label.grid(row=0, column=0, sticky=S) # sticky is directional position
+input_label.grid(row=0, column=0, sticky=W) # sticky is directional position.
 input_txt.grid(row=0, column=1)
 
 #combobox widget
+unit_label = Label(root, text="Convert to unit", font=font)
+unit_list = ["Fahrenheit", "Kelvin"]
+temp_combo = ttk.Combobox(root, value=unit_list, font=font, width=18)
+temp_combo.set("Kelvin") # Set default value.
 
+unit_label.grid(row=1, column=0, sticky=W)
+temp_combo.grid(row=1, column=1)
 #output widget
 
 #button widget
