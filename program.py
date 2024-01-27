212dd014c540bd32beb7a4f56e568f2b0b330ad6
Pun <impan_1611_2544@hotmail.com>
2024-01-27 15:29:23 +0700

    Add:input widget

--- program.py
+++ program.py
@@ -8,7 +8,12 @@ root.resizable(0, 0)
 #settings
 font = ("Arial", 15, "bold")
 color = "orange"
+
 #input widget
+input_label = Label(root, text="temperature(C)", font=font)
+input_txt = Entry(root, width=20, font=font)
+input_label.grid(row=0, column=0, sticky=S) # sticky is directional position
+input_txt.grid(row=0, column=1)
 
 #combobox widget
 
