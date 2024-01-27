d57f1e59cb878801b934d3e4e249cf0bc7989948
Pun <impan_1611_2544@hotmail.com>
2024-01-27 17:30:24 +0700

    Add:convert function

--- program.py
+++ program.py
@@ -6,6 +6,23 @@ root.title("Program temperature converter")
 root.iconbitmap("icon/tempicon.ico")
 root.resizable(0, 0)
 
+def convert():
+    output_txt.delete(0, END) # Clear text.
+
+    # Number as entered by a user.
+    celcius_value = float(input_txt.get())
+
+    # Unit as entered by a user.
+    unit_value = temp_combo.get()
+
+    # process
+    if unit_value == "Kelvin":
+        kelvin = celcius_value+273
+        output_txt.insert(0, kelvin)
+    else:
+        fahrenheit = (celcius_value*1.8)+32
+        output_txt.insert(0, fahrenheit)
+
 #settings
 font = ("Arial", 15, "bold")
 color = "orange"
@@ -26,12 +43,12 @@ temp_combo.grid(row=1, column=1)
 
 #output widget
 output_label = Label(root, text="Result", font=font)
-output_text = Entry(root, width=20, font=font)
+output_txt = Entry(root, width=20, font=font)
 output_label.grid(row=2, column=0, sticky=W)
-output_text.grid(row=2, column=1)
+output_txt.grid(row=2, column=1)
 
 #button widget
-convertBtn = Button(root, text="Convert", font=font, width=10, bg=color)
+convertBtn = Button(root, text="Convert", font=font, width=10, bg=color, command=convert)
 resetBtn = Button(root, text="Clear", font=font, width=7, bg=color)
 convertBtn.grid(row=3, column=1, sticky=W, padx=5, pady=5)
 resetBtn.grid(row=3, column=1, sticky=E, padx=5, pady=5)
